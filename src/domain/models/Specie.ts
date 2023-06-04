ffe115ea1faf979d8db10f3d7a97a632179a7c81
Leticia de Deus <leticia.carvalho@jcpm.com.br>
2023-06-04 11:41:21 -0300

    feat: Specie model

---
+++ src/domain/models/Specie.ts
@@ -0,0 +1,23 @@
+import { Entity } from '../../shared/domain/Entity'
+
+export interface SpecieProps {
+    colloquialName: string
+    scientificName: string
+}
+
+export class Specie extends Entity<SpecieProps> {
+
+    private constructor(props: SpecieProps, id?: string) {
+        super(props, id)
+    }
+
+    static create(props: SpecieProps): Specie {
+        const specie = new Specie(props)
+
+        return specie
+    }
+
+    static getValue(specie: Specie): SpecieProps {
+        return specie.props
+    }
+}
